7c1e23cc2f7fa7c88cd53ec0e4e2f2b06908d2ff
shakilmahammud <hshakil987@gmail.com>
2020-09-26 23:11:37 +0600

    pactices express js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
+const cros=require('cors');
+const user=['ami','tmi','se','amra','amader','hi']
 
+app.use(cros());
 app.get('/',(req,res)=>{
     res.send('thanks for call me');
 })
-app.listen(50000,()=>console.log('listening to port 5000'));
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.post('/adduser',(req,res)=>{
+    const user=req.body;
+    user.id=55;
+    res.send(user);
+    
+})
+app.get('/user/:id',(req,res)=>{
+    const id=req.params.id;
+    const name=user[id];
+    res.send({name,id});
+})
+
+app.listen(5000,()=>console.log('listening to port 5000'));
 
 
